27cf5dbdf36a1ccd82b76c0d76ec15da4e7b581c
gahyeon <1000ji1@naver.com>
2023-12-10 22:16:35 +0900

    refactor : change no static

--- src/main/java/racingcar/model/RandomNumber.java
+++ src/main/java/racingcar/model/RandomNumber.java
@@ -3,13 +3,16 @@ package racingcar.model;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RandomNumber {
-    private static int randomNum;
+    private int randomNum;
 
-    public static void make() {
+    public RandomNumber() {
+    }
+
+    public void make() {
         randomNum = pickNumberInRange(0, 9);
     }
 
-    public static boolean isForward() {
+    public boolean isForward() {
         if (randomNum >= 4) {
             return true;
         }
