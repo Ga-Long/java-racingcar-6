b60964447f7b3a2877b15bd47eb93904fd709c70
gahyeon <1000ji1@naver.com>
2023-12-10 23:13:31 +0900

    style : change static final

--- src/main/java/racingcar/model/Validator.java
+++ src/main/java/racingcar/model/Validator.java
@@ -4,35 +4,43 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Validator {
+    private static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "숫자이여야 합니다.";
+    private static final String POSITIVE_NUMBER_EXCEPTION_MESSAGE = "양수여야 합니다.";
+    private static final String NAME_SPACE_EXCEPTION_MESSAGE = "이름에 공백이 포함되어 있습니다.";
+    private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 5자 이하이어야 합니다.";
+    private static final String MINIMUM_PARTICIPANTS_EXCEPTION_MESSAGE = "참여자는 2명 이상이여야 합니다.";
+    private static final String NAME_DUPLICATE_EXCEPTION_MESSAGE = "중복된 이름이 존재합니다: ";
+
+
     public static void checkNumber(String attemptCount) {
         try {
             Integer.parseInt(attemptCount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자이여야 합니다.");
+            throw new IllegalArgumentException(NUMBER_FORMAT_EXCEPTION_MESSAGE);
         }
     }
 
     public static void isPositiveNum(int attemptCount) {
         if (attemptCount <= 0) {
-            throw new IllegalArgumentException("양수여야 합니다.");
+            throw new IllegalArgumentException(POSITIVE_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
     public static void isSpace(String name) {
         if (name.contains(" ")) {
-            throw new IllegalArgumentException("이름에 공백이 포함되어 있습니다.");
+            throw new IllegalArgumentException(NAME_SPACE_EXCEPTION_MESSAGE);
         }
     }
 
     public static void checkNameLength(String name) {
         if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하이어야 합니다.");
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 
     public static void checkMinimumParticipants(String[] names) {
         if (names.length < 2) {
-            throw new IllegalArgumentException("참여자는 2명 이상이여야 합니다.");
+            throw new IllegalArgumentException(MINIMUM_PARTICIPANTS_EXCEPTION_MESSAGE);
         }
     }
 
@@ -40,7 +48,7 @@ public class Validator {
         Set<String> uniqueNames = new HashSet<>();
         for (String name : names) {
             if (!uniqueNames.add(name)) {
-                throw new IllegalArgumentException("중복된 이름이 존재합니다: " + name);
+                throw new IllegalArgumentException(NAME_DUPLICATE_EXCEPTION_MESSAGE + name);
             }
         }
     }
